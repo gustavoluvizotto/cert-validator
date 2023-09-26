a531827d933817e5aa70c010a51f058f3837ec4f
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2023-09-26 16:37:32 +0200

    fix input arguments to cert validator container

--- run.sh
+++ run.sh
@@ -2,5 +2,12 @@
 
 SHARED_DIR="shared_dir"
 
-podman run --rm -v "$(pwd)"/"${SHARED_DIR}":/go/"${SHARED_DIR}" --name cert-validator cert-validator "$@"
+declare -a PARAMS=()
+while (( "$#" )); do
+    if [ -n "$1" ]; then
+        PARAMS+=("$1")
+    fi
+done
+
+podman run --rm -v "$(pwd)"/"${SHARED_DIR}":/go/"${SHARED_DIR}" --name cert-validator cert-validator "${PARAMS[@]}"
 
