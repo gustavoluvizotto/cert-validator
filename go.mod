1e70ee6353ae71260e48b11583f03207fb0d64e1
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2023-09-28 15:04:32 +0200

    doing intermediate certs permutation

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/gustavoluvizotto/cert-validator
 go 1.21.1
 
 require (
+	github.com/etnz/permute v0.0.0-20160126210303-99613134c393
 	github.com/rs/zerolog v1.31.0
 	github.com/xitongsys/parquet-go v1.6.2
 	github.com/xitongsys/parquet-go-source v0.0.0-20230919034749-0b16411e6349
@@ -11,6 +12,7 @@ require (
 require (
 	github.com/apache/arrow/go/arrow v0.0.0-20211112161151-bc219186db40 // indirect
 	github.com/apache/thrift v0.19.0 // indirect
+	github.com/etnz/stringset v0.0.0-20160903143730-b4da118f1741 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- validator/validator.go
+++ validator/validator.go
@@ -4,63 +4,85 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
+	"github.com/etnz/permute"
 	"github.com/gustavoluvizotto/cert-validator/input"
 	"github.com/gustavoluvizotto/cert-validator/result"
 	"github.com/rs/zerolog/log"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 )
 
-func ValidateChainPem(certChain input.CertChain, rootCAs *x509.CertPool, resultChan chan result.ValidationResult, scanDate time.Time) {
-	log.Debug().Int32("id", *certChain.Id).Msg("Validating certificate chain")
+var mtx sync.Mutex
 
+func ValidateChainPem(certChain input.CertChain, rootCAs *x509.CertPool, resultChan chan result.ValidationResult, scanDate time.Time) {
 	// rfc5280#section-4.2.1.12
-	keyUsage := make([]x509.ExtKeyUsage, x509.ExtKeyUsageServerAuth)
+	keyUsage := make([]x509.ExtKeyUsage, x509.ExtKeyUsageAny) // here we give a lower bound in our results
 	valResult := result.ValidationResult{Id: *certChain.Id, IsValid: false}
 	var leaf *x509.Certificate
 	var err error
 	isValid := false
-
 	// Iterate over the certificates in the chain, permuting the leaf certificate and the intermediate certificates,
-	// and verify the chain against the root CAs
+	// and verify the chain against the root CAs. Save all valid permutations
 	for i, certStr := range certChain.Chain {
 		leaf, err = getCertificateFromPEM(certStr)
 		if err != nil {
 			continue
 		}
-		intermediates := x509.NewCertPool()
-		for j, certStr2 := range certChain.Chain {
+
+		var intermediateIdx []int
+		for j, _ := range certChain.Chain {
 			if i != j {
-				if !intermediates.AppendCertsFromPEM([]byte(certStr2)) {
+				intermediateIdx = append(intermediateIdx, j)
+			}
+		}
+
+		var s [2]int
+		h := permute.NewHeap(len(intermediateIdx))
+		for h.Next(&s) {
+			permute.SwapInts(s, intermediateIdx)
+			intermediates := x509.NewCertPool()
+			for _, idx := range intermediateIdx {
+				if !intermediates.AppendCertsFromPEM([]byte(certChain.Chain[idx])) {
 					err = errors.New("failed to append intermediate certificate")
 					continue
 				}
 			}
-		}
-		// Build the certificate verification options
-		opts := x509.VerifyOptions{
-			Roots:         rootCAs,
-			CurrentTime:   scanDate,
-			Intermediates: intermediates,
-			DNSName:       "",
-			KeyUsages:     keyUsage,
-		}
+			if err != nil {
+				// could not append one of the intermediate certificates, meaning the cert is invalid
+				continue
+			}
+			// Build the certificate verification options
+			opts := x509.VerifyOptions{
+				Roots:         rootCAs,
+				CurrentTime:   scanDate,
+				Intermediates: intermediates,
+				DNSName:       "",
+				KeyUsages:     keyUsage,
+			}
 
-		// Verify the certificate chain
-		_, err = leaf.Verify(opts)
-		if err != nil {
-			continue
+			// Verify the certificate chain
+			_, err = leaf.Verify(opts)
+			if err != nil {
+				continue
+			}
+			isValid = true
+			break
 		}
-		isValid = true
-		break
+		if isValid {
+			break
+		}
 	}
 
 	valResult.IsValid = isValid
 	if err != nil {
 		valResult.ErrorData = err.Error()
 	}
+
 	resultChan <- valResult
+	log.Debug().Int32("id", *certChain.Id).Msg("Validated certificate chain")
+
 }
 
 func getCertificateFromPEM(certStr string) (*x509.Certificate, error) {
