8be3080f142344454b96738837e8f4df1a930b4f
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2023-10-16 10:06:16 +0000

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+INPUT_PARQUET=$1
+PORT=$2
+SCAN_DATE=$3
 SHARED_DIR="shared_dir"
 
-declare -a PARAMS=()
-while (( "$#" )); do
-    if [ -n "$1" ]; then
-        PARAMS+=("$1")
-    fi
-done
+# retrieve all root store files
+podman run --net=host --rm -v "$(pwd)"/${SHARED_DIR}:/app/${SHARED_DIR} rootstores-collect
 
-# show to the user to run it...
-echo "podman run --net=host --rm -v \"\$(pwd)\"/${SHARED_DIR}:/app/${SHARED_DIR} --name cert-validator cert-validator"
+# run validator
+podman run --net=host --rm -v "$(pwd)"/${SHARED_DIR}:/app/${SHARED_DIR} --name cert-validator cert-validator --input-parquet=${INPUT_PARQUET} --scan-date=${SCAN_DATE} --output=${SHARED_DIR}/output.parquet -v 2 --log-file=${SHARED_DIR}/log.json --rm
+
+# upload results
+podman run --net=host --rm -v "$(pwd)"/${SHARED_DIR}:/app/${SHARED_DIR} cert-validator-upload --log-file=${SHARED_DIR}/log.json --output=${SHARED_DIR}/output.parquet --port=${PORT} --scan-date=${SCAN_DATE}
 
