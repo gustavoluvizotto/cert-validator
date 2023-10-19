d0c45d4e695ddb45a2287be1dd8174146e9a3325
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2023-10-19 09:20:58 +0000

    fix apple s3 prefix

--- rootstores/apple.go
+++ rootstores/apple.go
@@ -2,5 +2,5 @@ package rootstores
 
 const (
 	AppleRootStoreFile     = "shared_dir/apple-keychain-certs.pem"
-	AppleS3RootStorePrefix = "rootstores/format=raw/store=apple/"
+	AppleS3RootStorePrefix = "rootstores/format=raw/store=apple"
 )
