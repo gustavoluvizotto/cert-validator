d7879a35674f7424d7e45351da3a60e637a5a0d9
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2023-10-23 13:20:50 +0200

    add how to retrieve files from windows vm

--- rootstores/collect/windows-rootstore.ps1
+++ rootstores/collect/windows-rootstore.ps1
@@ -8,3 +8,7 @@ certutil.exe -syncWithWU .\shared_dir\windows-rootstore\
 Compress-Archive -Path .\shared_dir\windows-rootstore\* -DestinationPath .\shared_dir\windows-rootstore.zip -Force
 
 rm -Recurse -Force .\shared_dir\windows-rootstore\
+
+# retrieve the file via:
+# scp windows:/users/gustavo/workspace/cert-validator/shared_dir/windows-rootstore.zip shared_dir/.
+
