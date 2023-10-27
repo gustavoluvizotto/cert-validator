d05ac63b5757d6694dcab4d3b667e63e663842e5
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2023-10-27 11:17:11 +0200

    remove cached files

--- rootstores/collect/others-rootstores.sh
+++ rootstores/collect/others-rootstores.sh
@@ -4,3 +4,7 @@ SHARED_DIR="shared_dir"
 
 # retrieve all others root store files
 podman run --net=host --rm -v "$(pwd)"/${SHARED_DIR}:/app/${SHARED_DIR} rootstores-collect --collect-others
+
+rm -f ${SHARED_DIR}/google_services_root.pem 
+rm -f ${SHARED_DIR}/IncludedRootsDistrustSMIMEPEM.csv
+rm -f ${SHARED_DIR}/IncludedRootsDistrustTLSSSLPEM.csv
