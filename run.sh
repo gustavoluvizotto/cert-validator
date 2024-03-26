0701f5550b2111d4533c592b513bb23b6020484c
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2024-03-26 14:56:38 +0100

    ignoring errors with sha1 signature

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ SHARED_DIR="shared_dir"
 podman run --net=host --rm -v "$(pwd)"/${SHARED_DIR}:/app/${SHARED_DIR} --name cert-validator cert-validator --prep --scan-date="${SCAN_DATE}" 
 
 # run validator
-podman run --net=host --rm -v "$(pwd)"/${SHARED_DIR}:/app/${SHARED_DIR} --name cert-validator cert-validator --input-parquet="${INPUT_PARQUET}" --scan-date="${SCAN_DATE}" --output=${SHARED_DIR}/output.parquet -v 2 --log-file=${SHARED_DIR}/log.json --rm
+podman run --net=host -e GODEBUG=x509sha1=1 --rm -v "$(pwd)"/${SHARED_DIR}:/app/${SHARED_DIR} --name cert-validator cert-validator --input-parquet="${INPUT_PARQUET}" --scan-date="${SCAN_DATE}" --output=${SHARED_DIR}/output.parquet -v 2 --log-file=${SHARED_DIR}/log.json --rm
 
 # upload results
 podman run --net=host --rm -v "$(pwd)"/${SHARED_DIR}:/app/${SHARED_DIR} cert-validator-upload --log-file=${SHARED_DIR}/log.json --output=${SHARED_DIR}/output.parquet --port="${PORT}" --scan-date="${SCAN_DATE}"
